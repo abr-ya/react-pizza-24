85fea555d6a69078cf1fc89fef6a0df98becfa07
abr-ya <gamer05@list.ru>
2024-01-04 01:25:05 +0300

    feat: Add Input component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,11 +3,11 @@ import styles from "./Button.module.css";
 import { ButtonProps } from "./Button.props";
 import cn from "classnames";
 
-const Button: FC<ButtonProps> = ({ children, className, appearence = "small", ...props }) => (
+const Button: FC<ButtonProps> = ({ children, className, variant = "small", ...props }) => (
   <button
     className={cn(styles["button"], styles["accent"], className, {
-      [styles["small"]]: appearence === "small",
-      [styles["big"]]: appearence === "big",
+      [styles["small"]]: variant === "small",
+      [styles["big"]]: variant === "big",
     })}
     {...props}
   >
