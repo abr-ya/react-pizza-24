c4b6934193a2513cb7d8b433e9c26132a9acde4f
abr-ya <gamer05@list.ru>
2024-01-12 08:32:38 +0300

    refactor: Add Zustand persist

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -1,7 +1,12 @@
 import { Heading } from "../../components";
+import { getCounterValue } from "../DemoStore/counter.store";
 
 const Cart = () => {
-  return <Heading>Заказать</Heading>;
+  return (
+    <>
+      <Heading>Заказать</Heading>А тут значение счётчика получаем без хука: {getCounterValue()}
+    </>
+  );
 };
 
 export default Cart;
--- src/pages/DemoStore/counter.store.ts
+++ src/pages/DemoStore/counter.store.ts
@@ -1,21 +1,28 @@
 import { create } from "zustand";
-import { devtools } from "zustand/middleware";
+import { devtools, persist } from "zustand/middleware";
 
 export interface ICounterState {
   value: number;
   increment: () => void;
   decrement: () => void;
   setValue: (value: number) => void;
+  tempString: string;
 }
 
 export const useCounter = create<ICounterState>()(
   devtools(
-    (set) => ({
-      value: 0,
-      increment: () => set((state) => ({ value: state.value + 1 }), false, "increment"),
-      decrement: () => set((state) => ({ value: state.value - 1 }), false, "decrement"),
-      setValue: (value) => set(() => ({ value }), false, "setValue"),
-    }),
+    persist(
+      (set) => ({
+        value: 0,
+        increment: () => set((state) => ({ value: state.value + 1 }), false, "increment"),
+        decrement: () => set((state) => ({ value: state.value - 1 }), false, "decrement"),
+        setValue: (value) => set(() => ({ value }), false, "setValue"),
+        tempString: "просто строка для тестов",
+      }),
+      { name: "CounterLS", partialize: (state) => ({ counterValue: state.value }) },
+    ),
     { name: "Counter" },
   ),
 );
+
+export const getCounterValue = () => useCounter.getState().value;
