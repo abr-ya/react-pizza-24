499e916a4bfeabe73ef674de7c5bc40034ee7459
abr-ya <gamer05@list.ru>
2024-01-19 21:18:47 +0300

    refactor: Add react-hook-form to Register page

--- src/pages/auth/Register.tsx
+++ src/pages/auth/Register.tsx
@@ -1,39 +1,59 @@
-import { FormEvent } from "react";
 import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
 
 import { Button, Heading, Input } from "../../components";
 
 import styles from "./Auth.module.css";
 
-export type RegisterForm = {
-  email: { value: string };
-  password: { value: string };
-  name: { value: string };
-};
+interface IFormData {
+  email: string;
+  password: string;
+  name: string;
+}
+
+const schema = yup.object({
+  email: yup.string().email("Email format is not valid").required("Email is required"),
+  password: yup.string().required("Password is required"),
+  name: yup.string().required("Name is required"),
+});
 
 const Register = () => {
-  const submitHandler = (e: FormEvent) => {
-    e.preventDefault();
-    const target = e.target as typeof e.target & RegisterForm;
-    const { email, password, name } = target;
-    console.log("Register", email, password, name);
+  const form = useForm<IFormData>({
+    defaultValues: {
+      email: "",
+      password: "",
+      name: "",
+    },
+    resolver: yupResolver(schema),
+  });
+
+  const { register, handleSubmit, formState } = form;
+  const { errors } = formState;
+
+  const onSubmit = (data: IFormData) => {
+    console.log(data);
   };
 
   return (
     <div className={styles.login}>
       <Heading>Регистрация</Heading>
-      <form className={styles.form} onSubmit={submitHandler}>
+      <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
         <div className={styles.field}>
           <label htmlFor="email">Ваш email</label>
-          <Input id="email" name="email" placeholder="Email" />
+          <Input id="email" placeholder="Email" {...register("email")} />
+          <p className="error">{errors.email?.message}</p>
         </div>
         <div className={styles.field}>
           <label htmlFor="password">Ваш пароль</label>
-          <Input id="password" name="password" type="password" placeholder="Пароль" />
+          <Input id="password" type="password" placeholder="Пароль" {...register("password")} />
+          <p className="error">{errors.password?.message}</p>
         </div>
         <div className={styles.field}>
           <label htmlFor="name">Ваше имя</label>
-          <Input id="name" name="name" placeholder="Имя" />
+          <Input id="name" placeholder="Имя" {...register("name")} />
+          <p className="error">{errors.name?.message}</p>
         </div>
         <Button variant="big">Зарегистрироваться</Button>
       </form>
